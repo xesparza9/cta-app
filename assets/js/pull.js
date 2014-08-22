67cb62dddd36f063e84bbcac709bb6dd780fee50
Xochitl Esparza <xesparza9@gmail.com>
2014-08-22 15:12:34 +0000

    Coding for all pages

--- assets/js/pull.js
+++ assets/js/pull.js
@@ -1,9 +1,25 @@
 //Pull.js file
 $.getJSON('http://data.cityofchicago.org/resource/8pix-ypme.json', function(lstops){
-  $('lstoplist').each();
+  $('#lstoplist').empty();
 
   $.each(lstops, function(i, lstop) {
     $('#lstoplist').append(generateLStopLink(lstop));
   });
     $('lstoplist').refresh();
-} )
+} ) 
+
+
+$.getJSON('http://data.cityofchicago.org/resource/mq3i-nnqe.json'), function(busstops){
+  $('#busstoplist').empty();
+  
+  $.each(busstops, function(i, busstop){
+    $('#busstoplist').append(generateBusStopLink(bustop));
+   });
+     $('busstoplist').refresh();
+  
+}
+
+
+
+
+
